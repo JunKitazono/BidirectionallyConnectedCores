56a3a61ba274823f0bc8ee1b4e5d9d9570f7294a
JunKitazono <kitazono@araya.org>
2021-10-18 20:23:47 +0900

    Add network diagrams

--- demos/ToyNetwork.m
+++ demos/ToyNetwork.m
@@ -9,13 +9,15 @@ type_of_mat = 1; % 1: Bidirectionality is considered, 2: Bidirectionality is ign
 
 if type_of_mat == 1
     W = ConnectionMatrix;
+    G = digraph(W);
 elseif type_of_mat == 2
     W = (ConnectionMatrix + ConnectionMatrix')/2;
+    G = graph(W);
 end
 
 
-%% Extract complexes
-% extract complexes
+%% %%%%%%%%%%%%%%%%%%%%%%% Extract Complexes %%%%%%%%%%%%%%%%%%%%%%%%%
+% Extract complexes
 [complexes, w_mc_complexes, main_complexes, w_mc_main_complexes, Res] = HPC( W );
 
 % Sort indices accotding to the hierarchical structure of complexes (See Fig. 6 in Kitazono et al., 2021)
@@ -23,8 +25,8 @@ end
 
 
 
-%%  Set figure parameters
-figPosition = 100+[0 0 300 300];
+%% %%%%%%%%%%%%%%%%%%%%%%% Visualize results %%%%%%%%%%%%%%%%%%%%%%%%%
+%  Set figure parameters
 mainSize = 0.80;
 subSize = 0.025;
 margin = (1-mainSize-subSize)/2;
@@ -33,27 +35,44 @@ gap_between_MainFigAndColorbar = 0.01;
 Position_mat = [margin margin mainSize mainSize];
 Position_clb = [margin+mainSize+gap_between_MainFigAndColorbar  margin subSize mainSize];
 
-%% Show the connection matrix
-figure
-set(gcf, 'Position', figPosition);
+%% Visualize with network diagrams
+% Network diagrams
+figure('Position', [50 350 250 200]);
+plot(G, 'XData', Xs, 'YData', Ys, 'NodeLabel', nodeNames, 'MarkerSize', 10, 'LineWidth', 2);
+
+% Complexes
+figure('Position', [300 350 250 200]);
+colormap(parula)
+plotComplexes(complexes, w_mc_complexes, '2D', 'Graph', G, 'XData', ...
+Xs, 'YData', Ys, 'NodeLabel', nodeNames, 'MarkerSize', 10, 'LineWidth', 2);
+
+figure('Position', [300 50 250 200]);
+colormap(parula)
+plotComplexes(complexes, w_mc_complexes, 'BirdsEye', 'Graph', G, 'XData', ...
+Xs, 'YData', Ys, 'NodeLabel', nodeNames, 'MarkerSize', 10, 'LineWidth', 2);
+zlabel('w^{mc}')
+
+
+%% Visualize with connection matrices
+% Connection matrix
+figure('Position', [550 350 250 250]);
 ax0 = axes('Position', Position_mat);
 imagesc(ax0, W);
 ax0.XTick = 1:10; ax0.YTick = 1:10;
 xticklabels(nodeNames), yticklabels(nodeNames)
 colormap(1-gray);
 colorbar(ax0, 'Position', Position_clb);
+title('Connection matrix')
 
 
-%% Visualize results
 % Visualize complexes by square areas.
-figure
-set(gcf, 'Position', figPosition);
+figure('Position', [800 350 250 250]);
 ax1 = axes('Position', Position_mat);
 ComplexDiagram(ax1, complexes, w_mc_complexes, Res);
 ax1.XTick = 1:10; ax1.YTick = 1:10;
 xticklabels(nodeNames(indices_sorted)), yticklabels(nodeNames(indices_sorted))
 colorbar(ax1, 'Position', Position_clb);
-
+title('Sorted connection matrix')
 % Superimpose the sorted connection matrix.
 ax2 = axes('Position', Position_mat);
 c = imagesc(ax2, W(indices_sorted, indices_sorted));
@@ -62,7 +81,7 @@ c.AlphaData = AlphaData;
 colormap(ax2, 1-gray);
 ax2.Visible = 'off';
 % The order of the rows and columns may be partially different from Fig. 7,
-% but this is not a problem (the extracted complexes are the same). 
+% but this is not a problem, i.e., the extracted complexes are the same. 
 % The order of the rows and columns is determined based on the hierarchical
 % partitioning. In this hierarchical partitioning, if there are multiple
 % min-cuts in a subnetwork, there is some arbitrariness in the order of
@@ -71,9 +90,9 @@ ax2.Visible = 'off';
 % extracted complexes. 
 
 
-% Coreness
+%% Coreness
 kv = coreness(complexes, w_mc_complexes, length(W));
-figure
+figure('Position', [1050 350 200 150]);
 plot(kv(indices_sorted), 'o')
 title('Coreness')
 xticks(1:length(W))
